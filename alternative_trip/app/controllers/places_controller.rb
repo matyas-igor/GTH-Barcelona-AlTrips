c2a09c3097c2b7c3790462b81b66f330b622929d
Erlinis Quintana <erlinis.quintana@tourlane.com>
2019-09-28 11:02:02 +0200

    Getting place details

--- alternative_trip/app/controllers/places_controller.rb
+++ alternative_trip/app/controllers/places_controller.rb
@@ -1,57 +1,20 @@
+# frozen_string_literal: true
+
+# ..
 class PlacesController < ApplicationController
-   def show
-     places = types.each_with_object([]) do |type, collection|
-       collection << client.spots(params[:lat], params[:lng],
-                                  radius: 3000,
-                                  types: [type],
-                                  exclude: exclude_types)
-     end.flatten
 
-     render json: places
-   end
+  def show
+    place = client.spot(params[:id])
 
-   private
+    render json: place
+  end
 
-   def client
-     @client ||= ::GooglePlaces::Client.new(
-       Rails.application.credentials.dig(:google, :api_key)
-     )
-   end
 
-   def types
-     [
-       'amusement_park',
-       'museum',
-       'park',
-       'tourist_attraction'
-     ]
-   end
+  private
 
-   def exclude_types
-     [
-       'bar',
-       'cafe',
-       'restaurant',
-       'department_store',
-       'moving_company',
-       'store',
-       'continent',
-       'country',
-       'finance',
-       'floor',
-       'food',
-       'general_contractor',
-       'geocode',
-       'health',
-       'intersection',
-       'locality',
-       'neighborhood',
-       'place_of_worship',
-       'political',
-       'post_box',
-       'place_of_worship',
-       'political',
-       'general_contractor'
-     ]
-   end
+  def client
+    @client ||= ::GooglePlaces::Client.new(
+      Rails.application.credentials.dig(:google, :api_key)
+    )
+  end
 end
--- alternative_trip/config/routes.rb
+++ alternative_trip/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/places', only: [:show], to: 'places#show'
+  # get 'places/:lat/:lng', to: 'places#show'
+
+  get '/places/:id', to: 'places#show'
+  get '/near_places', to: 'near_places#index'
 end
